b1eee4436030879dcc861619b85fe2c52e51f2b1
d3sc <vinary686@gmail.com>
2023-09-08 20:48:01 +0700

    add-features: fix responsive and add comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// deklarasi variable
 const gameContainer = document.querySelector(".container"),
   userResult = document.querySelector(".user_result img"),
   cpuResult = document.querySelector(".cpu_result img"),
@@ -6,47 +7,55 @@ const gameContainer = document.querySelector(".container"),
   scoreCpuUi = document.querySelector(".scoreCpu"),
   scorePlayerUi = document.querySelector(".scorePlayer");
 
-// let scoreCpu = localStorage.getItem("scoreCpu");
-// let scorePlayer = localStorage.getItem("scorePlayer");
-// scorePlayerUi.textContent = scorePlayer;
-// scoreCpuUi.textContent = scoreCpu;
-// console.log(scoreCpu);
-// console.log(scorePlayer);
-
+// membuat variable score permainan, yang dimulai dari 0
 let scorePlayer = 0;
 let scoreCpu = 0;
+
+// menampilkan score kedalam halaman score
 scorePlayerUi.textContent = scorePlayer;
 scoreCpuUi.textContent = scoreCpu;
 
+// mengulang image yang ada didalam class option image dengan foreach
 optionImages.forEach((image, index) => {
-  scorePlayerUi.textContent = scorePlayer;
-  scoreCpuUi.textContent = scoreCpu;
+  // membuat event listeners pada setiap image yang di foreach
   image.addEventListener("click", (e) => {
+    // setiap opsi image diberikan class active.
     image.classList.add("active");
 
+    // membuat proses loading
     userResult.src = cpuResult.src = "images/rock.png";
     result.textContent = "Wait...";
 
+    // menghapus index class active pada opsi gambar yang tidak dipilih oleh user
     optionImages.forEach((image2, index2) => {
       index !== index2 && image2.classList.remove("active");
     });
 
+    // memberikan class start pada container
     gameContainer.classList.add("start");
 
-    let time = setTimeout(() => {
+    // memberikan waktu untuk menampilkan hasil
+    setTimeout(() => {
+      // menghapus class start pada container
       gameContainer.classList.remove("start");
 
+      // memasang gambar yang kita pilih ke bagian gambar user result
       let imageSrc = e.target.querySelector("img").src;
       userResult.src = imageSrc;
 
+      // membuat penentuan random giliran bot
       let randomNumber = Math.floor(Math.random() * 3);
       let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+      // memasang gambar random yang sudah ditentukan bot
       cpuResult.src = cpuImages[randomNumber];
 
+      // R = Rock, P = Papper, S = Scissors, lalu dilanjutkan dengan langsung manggil array dan dimasukkan kedalam variable cpuValue
       let cpuValue = ["R", "P", "S"][randomNumber];
 
+      // R = Rock, P = Papper, S = Scissors, lalu dilanjutkan dengan langsung manggil array dan dimasukkan kedalam variable userValue
       let userValue = ["R", "P", "S"][index];
 
+      // membuat peraturan permainan, dengan object
       let outcomes = {
         RR: "Draw",
         RP: "Cpu",
@@ -59,23 +68,23 @@ optionImages.forEach((image, index) => {
         SP: "User",
       };
 
+      // mengecek status permainan
       let outComeValue = outcomes[userValue + cpuValue];
 
+      // mengatur score permainan
       if (outComeValue == "User") {
-        // localStorage.setItem("scoreCpu", parseInt(scoreCpu) + 1);
         scorePlayerUi.textContent = scorePlayer += 1;
-        console.log(scorePlayer);
       } else if (outComeValue == "Cpu") {
-        // localStorage.setItem("scorePlayer", parseInt(scorePlayer) + 1);
         scoreCpuUi.textContent = scoreCpu += 1;
-        console.log(scoreCpu);
       }
 
+      // menampilkan hasil permainan
       result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
     }, 2500);
   });
 });
 
+// membuat function reload page
 const restart = () => {
   location.reload();
 };
